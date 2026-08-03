4d2daa2c8f86bb011c1bd4c86ce64c5d5a0f4fb2
Madhavan2700 <165609524+Madhavan2700@users.noreply.github.com>
2026-08-03 15:20:39 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,29 +1,39 @@
-console.log("login.js loaded");
+console.log("Login JS loaded");
 
-document.getElementById("loginForm").addEventListener("submit", async function (event) {
+document.getElementById("loginForm").addEventListener("submit", async function(event){
 
     event.preventDefault();
+
+    console.log("Login button clicked");
 
     const regno = document.getElementById("regno").value;
     const dob = document.getElementById("dob").value;
 
-    const { data, error } = await window.supabaseClient
+
+    const { data, error } = await supabaseClient
         .from("profiles")
         .select("*")
         .eq("reg_no", regno)
         .eq("dob", dob);
 
-    if (error) {
-        alert("Database Error");
+
+    if(error){
         console.log(error);
+        alert("Database Error");
         return;
     }
 
-    if (data.length > 0) {
+
+    if(data.length > 0){
+
         alert("Login Successful");
-        window.location.href = "home.html";
-    } else {
+        window.location.href="home.html";
+
+    }
+    else{
+
         alert("Invalid Register Number or Date of Birth");
+
     }
 
 });
