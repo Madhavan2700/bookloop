2a0ca7a61cb624058af7fac9df4148510e0d8276
Madhavan2700 <165609524+Madhavan2700@users.noreply.github.com>
2026-08-03 16:06:04 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://jaosqglajbjchrlfgixc.supabase.co";
 
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imphb3NxZ2xhamJqY2hybGZnaXhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU3NDA2NTgsImV4cCI6MjEwMTMxNjY1OH0.TBOOLdFY-JdiIC-kUNJgZ6vMnx_dMV24WhswsOdWVM8";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_EpTzM9oTx9IgXaGjNfvRVw_uw-BRLfv";
 
 
 if (window.supabase && window.supabase.createClient) {
