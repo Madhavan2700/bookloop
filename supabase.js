0e83bff48609c3dd0a77686ffefd28ec62390301
Madhavan2700 <165609524+Madhavan2700@users.noreply.github.com>
2026-08-03 16:09:46 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,18 +2,9 @@ const SUPABASE_URL = "https://jaosqglajbjchrlfgixc.supabase.co";
 
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_EpTzM9oTx9IgXaGjNfvRVw_uw-BRLfv";
 
+window.supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
 
-if (window.supabase && window.supabase.createClient) {
-
-    window.supabaseClient = window.supabase.createClient(
-        SUPABASE_URL,
-        SUPABASE_PUBLISHABLE_KEY
-    );
-
-    console.log("Supabase connected");
-
-} else {
-
-    console.error("Supabase CDN not loaded");
-
-}console.log(window.supabaseClient);
+console.log("Supabase connected");
