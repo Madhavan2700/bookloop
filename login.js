e84606871ddde2f613fbcb0e1e2518bcb989e66a
Madhavan2700 <165609524+Madhavan2700@users.noreply.github.com>
2026-08-03 11:43:53 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,25 @@
-console.log("login.js loaded successfully");
+document.getElementById("loginBtn").addEventListener("click", async function () {
+
+    const regno = document.getElementById("regno").value;
+    const dob = document.getElementById("dob").value;
+
+    const { data, error } = await supabase
+        .from("profiles")
+        .select("*")
+        .eq("reg_no", regno)
+        .eq("dob", dob);
+
+    if (error) {
+        alert("Database Error");
+        console.log(error);
+        return;
+    }
+
+    if (data.length > 0) {
+        alert("Login Successful");
+        window.location.href = "home.html";
+    } else {
+        alert("Invalid Register Number or DOB");
+    }
+
+});
