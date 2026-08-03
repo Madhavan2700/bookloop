8c371ecd70319a973d1f1ccf7f38f32e5bac7106
Madhavan2700 <165609524+Madhavan2700@users.noreply.github.com>
2026-08-03 14:14:38 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,4 +2,8 @@ const supabaseUrl = "https://jaosqglajbjchrlfgixc.supabase.co";
 
 const supabaseKey = "sb_publishable_EpTzM9oTx9IgXaGjNfvRVw_uw-BRLfv";
 
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+const { createClient } = supabase;
+
+const client = createClient(supabaseUrl, supabaseKey);
+
+window.supabase = client;
